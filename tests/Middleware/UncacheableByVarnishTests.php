85b17927e150a13c86f2c21e1775b338353224c5
Richan Fongdasen <richan-fongdasen@users.noreply.github.com>
2018-06-25 16:05:33 +0700

    Add UncacheableByVarnish middleware (#3)
    [ci skip] [skip ci]

---
+++ tests/Middleware/UncacheableByVarnishTests.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace RichanFongdasen\Varnishable\Tests\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use RichanFongdasen\Varnishable\Middleware\UncacheableByVarnish;
+use RichanFongdasen\Varnishable\Tests\TestCase;
+
+class UncacheableByVarnishTests extends TestCase
+{
+    /**
+     * Cacheable by varnish middleware object.
+     *
+     * @var \RichanFongdasen\Varnishable\Middleware\UncacheableByVarnish
+     */
+    protected $middleware;
+
+    /**
+     * Setup the test environment
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->middleware = new UncacheableByVarnish;
+    }
+
+    /** @test */
+    public function it_can_handle_the_incoming_request_and_manipulate_the_response_headers()
+    {
+        $request = new Request;
+        $response = new Response;
+
+        $this->middleware->handle(
+            $request,
+            function (Request $request) use ($response) { return $response; }
+        );
+
+        $actual = $response->headers->get(\Varnishable::getConfig('uncacheable_header'));
+        $this->assertEquals('1', $actual);
+    }
+}
